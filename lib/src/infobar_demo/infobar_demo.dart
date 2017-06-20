f30fc2b194a88ae048cc06e12f7517cef9993e9b
Harsányi Tamás <tharsanyi@skawa.hu>
2017-06-20 15:25:58 +0200

    Initial Example

---
+++ lib/src/infobar_demo/infobar_demo.dart
@@ -0,0 +1,25 @@
+import 'dart:html';
+import 'package:angular2/core.dart';
+import 'package:skawa_components/src/components/infobar/infobar.dart';
+
+@Component(
+  selector: 'infobar-demo',
+  template: '''
+      <h3>Simple inforbar without icon</h3>
+      <skawa-infobar>No icon, no url</skawa-infobar>
+      <h3>Inforbar with icon</h3>
+      <skawa-infobar icon="info">Info icon, still no url</skawa-infobar>
+      <h3>Inforbar with url</h3>
+      <skawa-infobar icon="open_in_new" url="https://github.com">Clicking icon will redirect if URL is specified</skawa-infobar>
+      <h3>Infobar which alerting when the icon triggers</h3>
+      <skawa-infobar icon="lightbulb_outline" (trigger)="alert()">Can also subscribe to primary action with (trigger)</skawa-infobar>
+    ''',
+  directives: const [
+    SkawaInfobarComponent,
+  ],
+)
+class InfobarDemoApp {
+  alert() {
+    window.alert('Triggered!');
+  }
+}
---
+++ web/main.dart
@@ -0,0 +1,6 @@
+import 'package:angular2/platform/browser.dart';
+import 'package:skawa_components_example/src/app_component.dart';
+
+main() {
+  bootstrap(AppComponent);
+}
