86a99e8bf4ede3b511a90ac9488054f0295bccae
Harsányi Tamás <tharsanyi@skawa.hu>
2018-02-13 13:06:08 +0100

    (fix) extra plugin fixed

--- web/ckeditor/config.js
+++ web/ckeditor/config.js
@@ -1,4 +1,5 @@
 CKEDITOR.editorConfig = function (config) {
     config.language = "en";
-    config.extraPlugins = 'justify,colorbutton';
+    config.extraPlugins = 'dartlogo';
+    config.extraAllowedContent = "img{src};";
 }
